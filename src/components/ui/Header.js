688beb9e98edddfbf161a46d609b8eeac4f9eab0
@dtorres3 <dtorres@unifywebsolutions.com>
2020-04-16 21:36:11 +1000

    160420202136

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+import theme from './Theme';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -15,21 +17,34 @@ function ElevationScroll(props) {
     return React.cloneElement(children, {
       elevation: trigger ? 4 : 0,
     });
+};
+
+
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
   }
+}));
 
 
 const Header = (props) => {
+
+  const classes = useStyles();
+
     return (
+      <Fragment>
         <ElevationScroll {...props}>
             <AppBar position="fixed" color={"primary"}>
                 <ToolBar> 
-                <Typography variant="h4">
-                    Material UI
-                </Typography>
+                  <Typography variant="h4">
+                      Material UI
+                  </Typography>
                 </ToolBar>
             </AppBar>
         </ElevationScroll>
+        <div className={classes.toolbarMargin} />
+      </Fragment>
     )
 }
 
-export default Header
+export default Header;
